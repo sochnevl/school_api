1c4a9f9a0f53695d5df4f7b24a858a19ab006625
Alex <alexiamxsmart@gmail.com>
2024-02-07 23:57:06 +0300

    add: rescue status: :bad_request, instance variables to variables

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,27 +1,30 @@
 class StudentsController < ApplicationController
   # POST /students
   def create
-    @student = Student.new(student_params)
+    student = Student.new(student_params)
 
-    if @student.save
-      render json: @student, status: :created, location: @student
+    if student.save
+      render json: student, status: :created, location: student
     else
-      render json: @student.errors, status: :method_not_allowed
+      render json: student.errors, status: :method_not_allowed
     end
   end
 
   # DELETE /students/:id
   def destroy
-    @student = Student.find(params[:id])
-    @student.destroy
+    student = Student.find(params[:id])
+    student.destroy
+    render json: {}, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: {}, status: :bad_request
   end
 
   # GET /schools/:school_id/classes/:class_group_id/students
   def index
-    @class_group = ClassGroup.find(params[:class_group_id])
-    @students = @class_group.students
+    class_group = ClassGroup.find(params[:class_group_id])
+    students = class_group.students
 
-    render json: @students
+    render json: students
   end
 
   private
